6858591bf8778136352c09fa42902b464ad26c10
root <root@ip-172-31-12-43.ec2.internal>
2025-11-10 14:28:21 +0000

    java code

---
+++ app.java
@@ -0,0 +1,12 @@
+// HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World from Java!");
+
+        int number1 = 10;
+        int number2 = 20;
+        int sum = number1 + number2;
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
+
